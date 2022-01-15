d3df98d453bd93296b24b1219e891a2cfec21121
Miftahudin26 <miftahudinmahdi@gmail.com>
2022-01-15 14:44:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/appnewsindo/Api/NewsApi.java
@@ -0,0 +1,12 @@
+package com.example.appnewsindo.Api;
+
+public class NewsApi {
+    public static String GET_TOP_HEADLINES = "https://newsapi.org/v2/top-headlines?country=id&apiKey=6cda40eb94564afd8626ecd2f128e7a5";
+    public static String GET_CATEGORY_SPORTS = "https://newsapi.org/v2/top-headlines?country=id&category=sports&apiKey=6cda40eb94564afd8626ecd2f128e7a5";
+    public static String GET_CATEGORY_TECHNOLOGY = "https://newsapi.org/v2/top-headlines?country=id&category=technology&apiKey=6cda40eb94564afd8626ecd2f128e7a5";
+    public static String GET_CATEGORY_BUSINESS = "https://newsapi.org/v2/top-headlines?country=id&category=business&apiKey=6cda40eb94564afd8626ecd2f128e7a5";
+    public static String GET_CATEGORY_HEALTH = "https://newsapi.org/v2/top-headlines?country=id&category=health&apiKey=6cda40eb94564afd8626ecd2f128e7a5";
+    public static String GET_CATEGORY_ENTERTAINMENT = "https://newsapi.org/v2/top-headlines?country=id&category=entertainment&apiKey=6cda40eb94564afd8626ecd2f128e7a5";
+}
+
+// Apikey_f3aa09e2f1d7494e9fded68f30d4e4c1
